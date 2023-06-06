6658be5c5b896136351daa504b0ac184d0d911ed
jieun <balllling92@gmail.com>
2023-06-06 23:23:33 +0900

    가로 휠 스크롤 도입시 문제 해결 - 스크롤시 offsetTop에 위치 하지 않는 문제 해결 - 가로 스크롤이 끝나기 전에 푸터 부분이 스크롤 되는 문제 해결

--- js/scrollAni.js
+++ js/scrollAni.js
@@ -5,79 +5,39 @@ $(document).ready(function () {
     $(".title-box").clone().appendTo(".title-box-wrap");
   }
 
-
-  // ---------- 요소의 높이값 ----------
-
-  // #horizontal-wrap 스크롤 총 높이값
+  // 요소의 높이값, 높이값 지정
+  let main = $("#main-wrap").prop('scrollHeight');
+  let topw = $("#top-wrap").prop('scrollHeight');
   let horizontal = $("#horizontal-wrap").prop('scrollHeight');
-  console.log(horizontal)
 
-  // #colunm-wrap 스크롤 총 높이값
-  let colunm = $("#colunm-wrap").prop('scrollHeight');
-  console.log(colunm)
-
-  // #bottom-wrap 스크롤 총 높이값
-  let bottom = $("#bottom-wrap").prop('scrollHeight');
-  console.log(bottom)
-
-  // #footer-wrap 스크롤 총 높이값
-  let footer = $("#footer-wrap").prop('scrollHeight');
-  console.log(footer)
-
-  // #item-page의 개수
-  let itemPage = $(".item-page").length;
-  console.log(itemPage);
-
-  // 컨테이너 총 넓이
-  let containerWidth = $(window).outerWidth() * itemPage;
-  console.log(containerWidth)
-
-  // #page-container에 총 넓이값 지정
-  $("#bottom-wrap").css({ "width": containerWidth });
-  $(".item").css({ "width": containerWidth });
-
-  // horizontal colunm의 스크롤 높이 합
-  let Height = horizontal + colunm;
-  console.log(Height)
-
-  // horizontal colunm의 스크롤 높이 합
-  let Height2 = horizontal + colunm + bottom;
-  console.log(Height2)
-
-  // 전체 총 스크롤 높이값
-  let totalHeight = horizontal + colunm + bottom + footer;
-  console.log(totalHeight)
+  // 스크롤 높이 합
+  let Height = main + topw;
+  let Height2 = main + topw + horizontal;
 
   // 총 스크롤 높이값을 바디의 높이값으로 지정
   $("body").css({ "height": Height2 + $(window).innerHeight() });
 
-
-  // ---------- 요소에 top값 지정 ----------
-
   // top값 지정(최상단에 붙지 않기 위해)
-  $("#colunm-wrap").css({ "top": horizontal });
-
-  // top값 지정
-  $("#bottom-wrap").css({ "top": Height });
-
-  $("#footer-wrap").css({ "top": Height2 });
+  $("#top-wrap").css({ "top": main });
+  $("#horizontal-wrap").css({ "top": Height });
+  $("#bottom-wrap").css({ "top": Height2 });
 
 
   // ---------- 스크롤 함수 ----------
 
   let scrollAni = () => {
 
-    // 실시간 스크롤 top의 위치값
     let ScrollTop = $(document).scrollTop();
-    console.log(ScrollTop)
 
-    if (ScrollTop < horizontal) {
+    // ---------- 세로 스크롤 ---------- 
+
+    if (ScrollTop < main) {
 
       // 스크롤업시 최상단에 고정
-      $("#horizontal-wrap").css({ "top": 0 });
+      $("#main-wrap").css({ "top": 0 });
 
-      // 스크롤업시 #horizontal-wrap 하단에 고정
-      $("#colunm-wrap").css({ "top": horizontal });
+      // 스크롤업시 #main-wrap 하단에 고정
+      $("#top-wrap").css({ "top": main });
 
       // 메인 블렌딩 텍스트 애니메이션
       // 스크롤업시 탑값이 1이 되도록 지정
@@ -88,15 +48,14 @@ $(document).ready(function () {
     }
 
     // 요소의 위치 탑값 지정 => 현재 브라우저 높이 - (실시간 스크롤 탑값 - 이전 요소의 높이값)
-    if (ScrollTop >= horizontal) {
-      $("#horizontal-wrap").css({ "top": 0 - (ScrollTop - horizontal) });
-      $("#colunm-wrap").css({ "top": $(window).innerHeight() - (ScrollTop - horizontal) });
-      $("#bottom-wrap").css({ "top": $(window).innerHeight() - (ScrollTop - (horizontal + colunm)) });
-      $("#footer-wrap").css({ "top": $(window).innerHeight() - (ScrollTop - (horizontal + colunm + bottom)) });
+    if (ScrollTop >= main) {
+      $("#main-wrap").css({ "top": 0 - (ScrollTop - main) });
+      $("#top-wrap").css({ "top": $(window).innerHeight() - (ScrollTop - main) });
+      $("#horizontal-wrap").css({ "top": $(window).innerHeight() - (ScrollTop - Height) });
     }
 
     // intro section 상단 이미지, 텍스트 애니메이션
-    if (ScrollTop >= $("#colunm-wrap").offset().top - 80) {
+    if (ScrollTop >= $("#top-wrap").offset().top - 80) {
 
       // 이미지 투명도, 스케일 값 
       $(".intro-img").addClass("active");
@@ -110,7 +69,7 @@ $(document).ready(function () {
     }
 
     // intro section 겹친 중간 이미지 애니메이션
-    if (ScrollTop >= $("#colunm-wrap").offset().top + 354) {
+    if (ScrollTop >= $("#top-wrap").offset().top + 354) {
       $(".intro-img2").addClass("active");
       // 이미지 애니메이션
       $(".img-effect-wrap2").animate({ "width": "100%" }, 500, "easeInCubic", () => {
@@ -119,38 +78,50 @@ $(document).ready(function () {
     }
 
     // intro section 최하단 이미지 애니메이션
-
-    if (ScrollTop >= $("#colunm-wrap").offset().top + 1186) {
-      // 이미지 애니메이션
-      $(".intro-img4").addClass("active");
-      // 텍스트 애니메이션
-      $(".bottom-title").animate({ "top": 22, "opacity": 1 }, 1500, "easeOutCubic");
+    if (ScrollTop >= $("#top-wrap").offset().top + 1186) {
+      $(".intro-img4").addClass("active"); // 이미지 애니메이션
+      $(".bottom-title").animate({ "top": 22, "opacity": 1 }, 1500, "easeOutCubic"); // 텍스트 애니메이션
     }
 
-    // item section 배경 컬러 애니메이션
-    if (ScrollTop >= $("#bottom-wrap").offset().top) {
+
+    // ---------- 가로 휠 스크롤 도입 설정 ---------- 
+
+    if (ScrollTop >= $("#horizontal-wrap").offset().top) {
 
       if (ScrollTop >= Height && ScrollTop <= Height2) {
-        $("#bottom-wrap").css({ "top": 0, "height": $(window).innerHeight() });
+        // 스크롤 탑에 붙을 수 있게 조정
+        $("#horizontal-wrap").css({ "top": 0, "height": $(window).innerHeight() });
       }
 
-      $(".item").css({ "background": "#A2D9D9" });
-      $(".info-text-area").css({ "left": 0, "opacity": 1 });
+      // 도입 애니메이션 스크롤 다운
+      $(".item").css({ "background": "#A2D9D9" }); // 배경 색상
+      $(".info-text-area").css({ "left": 0, "opacity": 1 }); // 텍스트
+      // 이미지
       $(".item1").css({ "left": "200px", "opacity": 1 });
       $(".item-bg1").css({ "left": "-20px", "opacity": 1 });
-    } else {
+    }
+    
+    else {
+      // 도입 애니메이션 스크롤 업
       $(".item").css({ "background": "#050505" });
       $(".info-text-area").css({ "left": "-100px", "opacity": 0 });
       $(".item1").css({ "left": "600px", "opacity": 0 });
       $(".item-bg1").css({ "left": "200px", "opacity": 0 });
     }
 
+
+    // ---------- 다시 세로 스크롤(푸터) ----------
+
     if (ScrollTop >= Height2) {
-      $("body").css({ "height": totalHeight + $(window).innerHeight() }); // 총 스크롤 높이값 재설정
-      $("#footer-wrap").css({ "top": $(window).innerHeight() });
-    } else {
-      $("body").css({ "height": Height2 + $(window).innerHeight() });
+      // 푸터 탑값
+      $("#bottom-wrap").css({ "top": $(window).innerHeight() - (ScrollTop - (Height2)) });
+    } 
+    
+    else {
+      // 이전 높이값으로
+      $("body").css({ "height": Height2 + $(window).innerHeight() }); 
     }
+
   };
 
 
--- js/scrollSlide.js
+++ js/scrollSlide.js
@@ -2,73 +2,67 @@ $(document).ready(function () {
 
   // #item-page의 개수
   let itemPage = $(".item-page").length;
-  console.log(itemPage);
 
-  let scrollPageSlide = () => {
+  // 요소의 스크롤 높이값
+  let main = $("#main-wrap").prop('scrollHeight');
+  let topw = $("#top-wrap").prop('scrollHeight');
+  let horizontal = $("#horizontal-wrap").prop('scrollHeight');
+  let bottom = $("#bottom-wrap").prop('scrollHeight');
 
-    // 가로로 정렬
-    for (let i = 0; i < itemPage; i++) {
-      $("#page-container").children().eq(i).css({ "left": i * 100 + "%" });
-    }
+  // 전체 총 스크롤 높이값
+  let totalHeight = main + topw + horizontal + bottom;
 
-    // 컨테이너 총 넓이
-    let containerWidth = $(window).outerWidth() * itemPage;
-    console.log(containerWidth)
-
-    // #page-container에 총 넓이값 지정
-    $("#bottom-wrap").css({ "width": containerWidth });
-
-    // 컨테이너의 전체 넓이 / 7(총 페이지 수) = 페이지의 넓이값
-    $(".item-page").css({ "width": containerWidth / itemPage });
+  // 페이지 가로로 정렬
+  for (let i = 0; i < itemPage; i++) {
+    $("#page-container").children().eq(i).css({ "left": i * 100 + "%" });
   }
 
-  scrollPageSlide();
-
-
-  // 현재 페이지의 x축 위치값 배열
+  // 페이지의 x축 위치값 배열
   let array = [];
   for (let i = 0; i < itemPage; i++) {
     array[i] = $(".item-page").eq(i).offset().left;
   }
 
-  console.log(array)
-
-  let array2 = [];
-  for (let i = 0; i < itemPage; i++) {
-    array2[i] = $(".item-page").eq(i).offset().top;
-  }
-
-  console.log(array2)
-
-
-
   // 배경 색상 배열
-  let colors = ["#A2D9D9", "#EFE3BA", "#EDD1B7", "#EBDDCD", "#EED2C9", "#D1BDDB", "#BABEE5"]
+  let colors = [
+    "#A2D9D9", 
+    "#EFE3BA", 
+    "#EDD1B7", 
+    "#EBDDCD", 
+    "#EED2C9", 
+    "#D1BDDB", 
+    "#BABEE5"]
 
-  // 마우스 휠, 스크롤 이벤트
+
+  // ---------- 마우스 휠, 스크롤 이벤트 ----------
+
   $(".item-page").on("scroll mousewheel DOMMouseScroll", function (e) {
+
     let ScrollTop = $(document).scrollTop();
-
-    let ScrollLeft = $(document).scrollLeft() + innerWidth;
-    console.log(ScrollLeft);
-
-
+    let ScrollLeft = $("#page-container").offset().left;
     let w_delta = e.originalEvent.wheelDelta;
 
     // 휠 아래로
-    if (w_delta < 0 && $(this).next().length < 7) {
-      if (ScrollTop >= $("#bottom-wrap").offset().top) {
+    if (w_delta < 0 && $(this).next().length > 0) {
+      if (ScrollTop >= $("#horizontal-wrap").offset().top) {
         e.preventDefault(); // 기본 동작 막기
-        $("#page-container").stop().animate({ "left": -array[$(this).index() + 1] }, 1000);
-        $(".item").css({ "background": colors[$(this).index() + 1] });
+        $("#page-container").stop().animate({ "left": -array[$(this).index() + 1] }, 1000); // 페이지 이동
+        $(".item").css({ "background": colors[$(this).index() + 1] }); // 배경 색상 변경
+      }
+
+      if (ScrollLeft <= -9000) {
+        // 총 스크롤 높이값 재설정(footer추가)
+        $("body").css({ "height": totalHeight + $(window).innerHeight() }); 
       }
     }
 
     // 휠 위로
     else if (w_delta > 0 && $(this).prev().length > 0) {
-      e.preventDefault();
-      $("#page-container").stop().animate({ "left": -array[$(this).index() - 1] }, 1000);
-      $(".item").css({ "background": colors[$(this).index() - 1] });
+      if (ScrollTop <= $("#horizontal-wrap").offset().top) {
+        e.preventDefault();
+        $("#page-container").stop().animate({ "left": -array[$(this).index() - 1] }, 1000);
+        $(".item").css({ "background": colors[$(this).index() - 1] });
+      }
     }
   });
 
@@ -77,7 +71,5 @@ $(document).ready(function () {
     for (let i = 0; i < $('.item-page').length; i++) {
       array[i] = $('.item-page').eq(i).offset().left
     }
-
-    scrollPageSlide();
   });
 });
